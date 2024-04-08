25e98d65b3b9fab28918c6549b7f39bbc1b56e1f
Drorre55 <dror604604@gmail.com>
2024-04-08 16:30:24 +0300

    Initial commit

---
+++ FunctionExOne/FunctionExOne1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunctionExOne.Abstract;
+
+namespace FunctionExOne
+{
+    public class FunctionExOne1 : IExercise
+    {
+        public int GetLastDigit(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAllButLastDigit(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAmountOfDigits(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDigintInIndex(int number, int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
